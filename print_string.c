42089b7a627486876385f484d587e46a1ad8e3e0
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-06-27 06:38:27 -0700

    if string is NULL

--- print_string.c
+++ print_string.c
@@ -10,14 +10,13 @@ int print_string(char *str)
 {
 	int count = 0, i = 0;
 
-	if (str != NULL)
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] != '\0')
 	{
-		while (str[i] != '\0')
-		{
-			print_char(str[i]);
-			count++;
-			i++;
-		}
+		print_char(str[i]);
+		count++;
+		i++;
 	}
 	return (count);
 }
