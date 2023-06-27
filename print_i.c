c9f98c4210bbfd9831b4d1a0c09c2d5d91f38ca3
Kevin Musembi <kevinmusembi.m@gmail.com>
2023-06-27 10:36:19 -0700

    Print Function

---
+++ print_i.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_i - print integer
+* @num: input
+*
+* Return: nothing
+*/
+void print_i(int num)
+{
+	int i, rem, n = 0;
+	int arr[50];
+
+	if (num == 0)
+		print_char('0' + 0);
+	if (num < 0)
+		print_char('-');
+	for (i = 0; num != 0; i++)
+	{
+		if (num < 0)
+			num *= -1;
+		rem = num % 10;
+		arr[i] = rem;
+		num /= 10;
+		n++;
+	}
+	for (i = n; i > 0; i--)
+		print_char('0' + arr[i - 1]);
+}
